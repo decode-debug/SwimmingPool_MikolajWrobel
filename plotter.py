e7ef2a4b0635edca3cae647fce0d1ba332813db9
decode-debug <mikolaj.wrobel.pl@gmail.com>
2024-12-16 23:45:29 +0100

    adding classes and main template

---
+++ plotter.py
@@ -0,0 +1,8 @@
+import swimming_pool_class
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ swimming_pool_class.py
@@ -0,0 +1,23 @@
+class Swimming_pool:
+    pass
+
+class Client:
+    pass
+
+class Tickets:
+    pass
+
+class Aviability_and_prices:
+    pass
+
+class TimeTable(Aviability_and_prices):
+    pass
+
+class Finance_raport:
+    pass
+
+
+class user_friendly_interface:
+    pass
+
+
